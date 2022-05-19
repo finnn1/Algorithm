6e47b31f38af48cbf0a1d2dd63cce22f4447d302
finnn1 <tgss3535@gmail.com>
2022-05-19 21:46:16 +0900

    220519-CLASS1_2577 - while로 구현

--- solved.ac/220519-CLASS1_2577/main.swift
+++ solved.ac/220519-CLASS1_2577/main.swift
@@ -1,5 +1,13 @@
 var inputs: [Int] = []
 var resultArray = [Int](repeating: 0, count: 10)
+
 for _ in 0..<3 { inputs.append(Int(readLine()!)!) }
-String(inputs.reduce(1, *)).forEach { resultArray[Int(String($0))!] += 1 }
+
+var reducedInputed = inputs.reduce(1, *)
+
+while reducedInputed > 0 {
+    resultArray[reducedInputed % 10] += 1
+    reducedInputed /= 10
+}
+
 resultArray.forEach { print($0) }
